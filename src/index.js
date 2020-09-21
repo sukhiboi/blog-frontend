107db00ff2b1ba7cd5e185f6e1ef530bfbf5f1a6
sukhiboi <40167376+sukhiboi@users.noreply.github.com>
2020-09-21 19:53:55 +0530

    created a react app

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <span>Hello World</span>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
