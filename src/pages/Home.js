22cdfcf0de5eac145ccb17de2d456235f488e72b
sukhiboi <40167376+sukhiboi@users.noreply.github.com>
2020-09-21 22:04:00 +0530

    added key props while rendering the post on homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,8 +4,8 @@ import Post from './../components/Post';
 
 const Home = props => {
   const posts = props.posts.map(post => (
-    <Link to={`/post/${post.id}`}>
-      <Post post={post} key={post.id} />
+    <Link to={`/post/${post.id}`} key={post.id}>
+      <Post post={post} />
     </Link>
   ));
   return <div>{posts}</div>;
