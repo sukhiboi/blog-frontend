f078b32c93c332028794fde07ed3610bd293e4cd
sukhiboi <40167376+sukhiboi@users.noreply.github.com>
2020-09-22 11:46:04 +0530

    updated the Lgogin page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import React from 'react';
+import Button from './../components/Button';
 
 const Login = props => {
-    return <a href={process.env.REACT_APP_LOGIN_REDIRECT}>Login with GitHub</a>;
-}
+  return (
+    <div className='vertical-center full-height'>
+      <span className='app-title'>Welcome to Blog</span>
+      <a className='link login-btn' href={process.env.REACT_APP_LOGIN_REDIRECT}>
+        <Button text='Login with Github' />
+      </a>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
