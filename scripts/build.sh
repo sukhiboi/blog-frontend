c694805694e7c7691136b0e23748eb221a621906
sukhiboi <40167376+sukhiboi@users.noreply.github.com>
2020-09-26 22:01:06 +0530

    udpdated build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,41 +1,33 @@
 #removing unwanted directories and files
 rm -rf node_modules public src lib test package.json package-lock.json
 
-pwd
-
 #cloning frontend
-git clone https://github.com/sukhiboi/blog-frontend.git;
+git clone https://github.com/sukhiboi/blog-frontend.git frontend;
 
 #creating react build
-cd blog-frontend;
+cd frontend;
 npm install;
 npm test;
 npm run build;
-
-#cloning build to root
-mv build ../build;
-
-#deleting frontend
-pwd
 cd ..;
-pwd
-rm -rf blog-frontend;
-ls
 
 #cloning backend
-git clone https://github.com/sukhiboi/blog-backend.git;
+git clone https://github.com/sukhiboi/blog-backend.git backend;
 
-cd blog-backend;
+cd backend;
 npm install;
 npm test;
+
+#cloning build to root
+mv ./../frontent/build ./build;
 rm -rf node_modules;
+cd ..;
 
 #cloning backend to root
-cd ..;
-ls
-cp -R ./blog-backend/* .;
+cp -R ./backend/* .;
 
 #deleting backend
-rm -rf blog-backend;
+rm -rf backend;
+rm -rf frontend;
 
-ls
+echo 'here\n' && ls
