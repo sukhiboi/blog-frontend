bde9a9cb36cb6ad9270b31fd1734c5bf52332405
sukhiboi <40167376+sukhiboi@users.noreply.github.com>
2020-09-26 17:12:54 +0530

    added travis support

---
+++ scripts/build.sh
@@ -0,0 +1,32 @@
+#cloning frontend
+git clone https://github.com/sukhiboi/blog-frontend.git;
+
+#creating react build
+cd blog-frontend;
+npm install;
+npm test;
+npm run build;
+
+#cloning build to root
+mv build ../build;
+
+#deleting frontend
+cd ..;
+rm -rf blog-frontend;
+
+#cloning backend
+git clone https://github.com/sukhiboi/blog-backend.git;
+
+cd blog-backend;
+npm install;
+npm test;
+rm -rf node_modules;
+
+#cloning backend to root
+cp -R ./* ..;
+
+#deleting backend
+cd ..;
+rm -rf blog-backend;
+
+rm -rf node_modules;
