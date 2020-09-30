5e08238fe88bc433efcc322bfe4d3fc1b3aaf94f
sukhiboi <40167376+sukhiboi@users.noreply.github.com>
2020-10-01 02:52:46 +0530

    made Button a styled component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const SimpleButton = styled.button`
+  padding: 10px 1rem;
+  border: 1px solid #ccc;
+  background: transparent;
+  border-radius: 4px;
+  font-size: 1rem;
+  outline: none;
+  width: fit-content;
+  align-self: flex-end;
+
+  &:hover {
+    cursor: pointer;
+    border: 1px solid #000;
+  }
+`;
 
 const Button = props => {
-  const className = `btn ${props.className}`;
-  return (
-    <button className={className} onClick={props.onClick}>
-      {props.text}
-    </button>
-  );
+  return <SimpleButton onClick={props.onClick}>{props.text}</SimpleButton>;
 };
 
 export default Button;
