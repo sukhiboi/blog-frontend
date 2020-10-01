52fea81c662270c8b0eaa620f423bcc4a98e5954
sukhiboi <40167376+sukhiboi@users.noreply.github.com>
2020-10-01 13:58:32 +0530

    made Loader a styled component

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,8 +1,18 @@
 import React from 'react';
-import './../styles/loader.css';
+import styled from 'styled-components';
+
+const LoaderLayout = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
+  height: 100vh;
+  font-size: 2rem;
+  font-weight: 900;
+`;
 
 const Loader = props => {
-  return <div className='loader'>Loading...</div>;
+  return <LoaderLayout children='Loading...' />;
 };
 
 export default Loader;
