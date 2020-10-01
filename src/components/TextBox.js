72b9d8422b98d20ab0c4ef7b2dbd9531cdc9ab5d
sukhiboi <40167376+sukhiboi@users.noreply.github.com>
2020-10-01 14:04:53 +0530

    made textbox a styled component

--- src/components/TextBox.js
+++ src/components/TextBox.js
@@ -1,12 +1,26 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
+
+const TextAreaLayout = styled.textarea`
+  font-family: sans-serif;
+  border: 1px solid #ccc;
+  outline: none;
+  padding: 10px;
+  resize: none;
+  border-radius: 4px;
+  margin-bottom: 10px;
+
+  &:focus {
+    border: 1px solid #000;
+  }
+`;
 
 const TextBox = props => {
   const [input, setInput] = useState(props.value || '');
 
   return (
-    <textarea
+    <TextAreaLayout
       {...props}
-      className={`input ${props.className}`}
       value={input}
       onChange={({ target }) => {
         setInput(target.value);
